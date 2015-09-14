9ddbf72bf2c37c049912f277ef6d95c465a07779
bossgivol <bossgivol@gmail.com>
2015-09-14 08:58:13 -0400

    Here is my Project

--- caughtSpeeding/src/caughtSpeeding/Speeding.java
+++ caughtSpeeding/src/caughtSpeeding/Speeding.java
@@ -11,6 +11,14 @@ public class Speeding {
 	
 	public static int caughtSpeeding(int speed, boolean isBirthday)
 	{
+		if(speed <= 60 && isBirthday == false || isBirthday == true && speed <= 65){
+			return 0;
+		} else if(speed <= 80 && speed >= 61 && isBirthday == false || speed <= 85 && speed >= 66 && isBirthday == true) {
+			return 1;
+		} else if(isBirthday == false && speed >= 81 || isBirthday == true && speed >= 86){
+			return 2;
+		}
 		return 0;
+			
 	}
 }
